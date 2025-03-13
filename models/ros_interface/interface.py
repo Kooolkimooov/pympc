43a348e77c7ee7d3d6194d41f85a68a5e583b3b6
Martin Filliung <martin-filliung@etud.univ-tln.fr>
2025-03-13 10:01:24 +0100

    changes to interface to reflect new base dynamics
    and implementation in Turtlebot

---
+++ models/ros_interface/interface.py
@@ -0,0 +1,62 @@
+from numpy import ndarray
+
+from geometry_msgs.msg import Pose
+
+
+class Interface:
+  _command_type = None
+  _initial_state = None
+
+  @staticmethod
+  def ros_pose_from_state( state: ndarray ) -> Pose: 
+    """
+    converts a numpy array to a ROS Pose message
+
+    :param state: numpy array represeting the state
+    :returns: Pose message
+    """
+    raise NotImplementedError()
+
+  @staticmethod
+  def pose_from_ros_pose( ros_pose: Pose ) -> ndarray: 
+    """
+    converts a ROS Pose message to a numpy array
+    
+    :param ros_pose: Pose message
+    :returns: numpy array represeting the state
+    """
+    raise NotImplementedError()
+
+  @staticmethod
+  def actuation_from_ros_actuation( ros_actuation: any ) -> ndarray: 
+    """
+    converts a ROS message of type command_type to a numpy array
+
+    :param ros_actuation: command_type message 
+    :returns: numpy array represeting the actuation
+    """
+    raise NotImplementedError()
+
+  @staticmethod
+  def ros_actuation_from_actuation( actuation: ndarray ) -> any: 
+    """
+    converts a numpy array to a ROS message of type command_type
+
+    :param actuation: numpy array represeting the actuation
+    :returns: command_type message
+    """
+    raise NotImplementedError()
+
+  @property
+  def command_type( self ) -> any: 
+    """
+    The type of the ros message used to send commands to the robot
+    """
+    return self._command_type
+  
+  @property
+  def initial_state( self )  -> ndarray:
+    """
+    the initial state of the robot
+    """
+    return self._initial_state
--- models/ros_interface/turtlebot.py
+++ models/ros_interface/turtlebot.py
@@ -2,14 +2,17 @@ from numpy import array, ndarray, pi, zeros
 from scipy.spatial.transform import Rotation
 
 from geometry_msgs.msg import Pose, Twist
-from .base_interface import BaseInterface
+from .interface import Interface
 from ..dynamics.turtlebot import Turtlebot
 
+# instanciation to be able to use the properties
+Turtlebot = Turtlebot()
 
-class TurtlebotROSInterface( BaseInterface ):
 
-  command_type = Twist
-  initial_state = zeros( (6,) )
+class TurtlebotROSInterface( Interface ):
+
+  _command_type = Twist
+  _initial_state = zeros( (6,) )
 
   max_linear_speed = 0.65
   max_angular_speed = pi
@@ -17,10 +20,10 @@ class TurtlebotROSInterface( BaseInterface ):
   @staticmethod
   def ros_pose_from_state( state: ndarray ) -> Pose:
     pose = Pose()
-    pose.position.x = state[ 0 ]
-    pose.position.y = state[ 1 ]
+    pose.position.x = state[ Turtlebot.position ][ 0 ]
+    pose.position.y = state[ Turtlebot.position ][ 1 ]
 
-    quaternion = Rotation.from_euler( 'xyz', array( [ 0, 0, state[ 2 ] ] ) ).as_quat()
+    quaternion = Rotation.from_euler( 'xyz', array( [ 0, 0, state[ Turtlebot.orientation ] ] ) ).as_quat()
     pose.orientation.x = quaternion[ 0 ]
     pose.orientation.y = quaternion[ 1 ]
     pose.orientation.z = quaternion[ 2 ]
@@ -30,29 +33,29 @@ class TurtlebotROSInterface( BaseInterface ):
 
   @staticmethod
   def pose_from_ros_pose( ros_pose: Pose ) -> ndarray:
-    pose = zeros( (Turtlebot.pose_size,) )
-    pose[ 0 ] = ros_pose.position.x
-    pose[ 1 ] = ros_pose.position.y
-    pose[ 2 ] = Rotation.from_quat(
+    pose = zeros( (Turtlebot.state_size // 2,) )
+    pose[ Turtlebot.position ][ 0 ] = ros_pose.position.x
+    pose[ Turtlebot.position ][ 1 ] = ros_pose.position.y
+    pose[ Turtlebot.orientation ] = Rotation.from_quat(
         [ ros_pose.orientation.x, ros_pose.orientation.y, ros_pose.orientation.z, ros_pose.orientation.w ]
-        ).as_euler( 'xyz' )[2]
+        ).as_euler( 'xyz' )[ 2 ]
     return pose
 
   @staticmethod
   def actuation_from_ros_actuation( ros_actuation: any ) -> ndarray:
     actuation = zeros( (Turtlebot.actuation_size,) )
 
-    actuation[ 0 ] = ros_actuation.linear.x
-    actuation[ 1 ] = ros_actuation.angular.z
+    actuation[ Turtlebot.linear_actuation ] = ros_actuation.linear.x
+    actuation[ Turtlebot.angular_actuation ] = ros_actuation.angular.z
 
-    if actuation[ 0 ] > TurtlebotROSInterface.max_linear_speed:
-      actuation[ 0 ] = TurtlebotROSInterface.max_linear_speed
-    elif actuation[ 0 ] < -TurtlebotROSInterface.max_linear_speed:
-      actuation[ 0 ] = -TurtlebotROSInterface.max_linear_speed
-    if actuation[ 1 ] > TurtlebotROSInterface.max_angular_speed:
-      actuation[ 1 ] = TurtlebotROSInterface.max_angular_speed
-    elif actuation[ 1 ] < -TurtlebotROSInterface.max_angular_speed:
-      actuation[ 1 ] = -TurtlebotROSInterface.max_angular_speed
+    if actuation[ Turtlebot.linear_actuation ] > TurtlebotROSInterface.max_linear_speed:
+      actuation[ Turtlebot.linear_actuation ] = TurtlebotROSInterface.max_linear_speed
+    elif actuation[ Turtlebot.linear_actuation ] < -TurtlebotROSInterface.max_linear_speed:
+      actuation[ Turtlebot.linear_actuation ] = -TurtlebotROSInterface.max_linear_speed
+    if actuation[ Turtlebot.angular_actuation ] > TurtlebotROSInterface.max_angular_speed:
+      actuation[ Turtlebot.angular_actuation ] = TurtlebotROSInterface.max_angular_speed
+    elif actuation[ Turtlebot.angular_actuation ] < -TurtlebotROSInterface.max_angular_speed:
+      actuation[ Turtlebot.angular_actuation ] = -TurtlebotROSInterface.max_angular_speed
 
     return actuation
 
@@ -60,7 +63,7 @@ class TurtlebotROSInterface( BaseInterface ):
   def ros_actuation_from_actuation( actuation: ndarray ) -> any:
     ros_actuation = TurtlebotROSInterface.command_type()
 
-    ros_actuation.linear.x = actuation[ 0 ]
-    ros_actuation.angular.z = actuation[ 1 ]
+    ros_actuation.linear.x = actuation[ Turtlebot.linear_actuation ]
+    ros_actuation.angular.z = actuation[ Turtlebot.angular_actuation ]
 
     return ros_actuation
