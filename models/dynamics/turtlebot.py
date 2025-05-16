9e9169bc36da5a70e27f0248d7862bc6e21d0cf7
Martin Filliung <martin-filliung@etud.univ-tln.fr>
2025-05-16 11:03:02 +0200

    propagating chagnes + testing

--- models/dynamics/turtlebot.py
+++ models/dynamics/turtlebot.py
@@ -1,40 +1,71 @@
-from numpy import cos, ndarray, pi, sin, zeros
+from numpy import cos, ndarray, pi, r_, sin, zeros
 
 from pympc.models.dynamics.dynamics import Dynamics
 
 
 class Turtlebot( Dynamics ):
-  """
-  implementation of the dynamics of the Turtlebot model
-  """
+    """
+    Implements the dynamics of the Turtlebot model
+    """
 
-  
+    _state_size = 6
+    _actuation_size = 2
+
+    _position = r_[ slice( 0, 2 ) ]
+    _orientation = r_[ 2 ]
+    _velocity = r_[ slice( 3, 5 ) ]
+    _body_rates = r_[ 5 ]
+
+    _linear_actuation = r_[ 0 ]
+    _angular_actuation = r_[ 1 ]
+
+    def __call__( self, state: ndarray, actuation: ndarray, perturbation: ndarray ) -> ndarray:
+        xdot = zeros( (6,) )
+        xdot[ 0 ] = cos( state[ 2 ] ) * actuation[ 0 ]
+        xdot[ 1 ] = sin( state[ 2 ] ) * actuation[ 0 ]
+        xdot[ 2 ] = actuation[ 1 ]
+
+        return xdot
+
+    def compute_error( self, actual: ndarray, target: ndarray ) -> ndarray:
+        error = zeros( actual.shape )
+        error[ :, :, :2 ] = actual[ :, :, :2 ] - target[ :, :, :2 ]
+        error[ :, :, 2 ] = (actual[ :, :, 2 ] - target[ :, :, 2 ]) % pi
+
+        return error
 
 
+if __name__ == '__main__':
+    from numpy import set_printoptions
+    from numpy.random import random
 
-  _state_size = 6
-  _actuation_size = 2
-  _perturbation_size = 0
+    set_printoptions( precision = 2, linewidth = 10000, suppress = True )
 
-  _position = slice( 0, 2 )
-  _orientation = 2
-  _velocity = slice( 3, 5 )
-  _body_rates = 5
+    tb = Turtlebot()
 
-  _linear_actuation = 0
-  _angular_actuation = 1
+    print( f"{tb.state_size=}" )
+    print( f"{tb.actuation_size=}" )
+    print( f"{tb.position=}" )
+    print( f"{tb.orientation=}" )
+    print( f"{tb.velocity=}" )
+    print( f"{tb.body_rates=}" )
+    print( f"{tb.linear_actuation=}" )
+    print( f"{tb.angular_actuation=}" )
 
-  def __call__( self, state: ndarray, actuation: ndarray, perturbation: ndarray ) -> ndarray:
-    xdot = zeros( (6,) )
-    xdot[ 0 ] = cos( state[ 2 ] ) * actuation[ 0 ]
-    xdot[ 1 ] = sin( state[ 2 ] ) * actuation[ 0 ]
-    xdot[ 2 ] = actuation[ 1 ]
+    s = random( (tb.state_size,) )
 
-    return xdot
+    print( f"{s=}" )
 
-  def compute_error( self, actual: ndarray, target: ndarray ) -> ndarray:
-    error = zeros( actual.shape )
-    error[ :, :, :2 ] = actual[ :, :, :2 ] - target[ :, :, :2 ]
-    error[ :, :, 2 ] = (actual[ :, :, 2 ] - target[ :, :, 2 ]) % pi
+    a = random( (tb.actuation_size,) )
+    print( f"{a=}" )
 
-    return error
+    p = random( (tb.state_size // 2,) )
+    print( f"{p=}" )
+
+    ds = tb( s, a, p )
+    print( f"{ds=}" )
+
+    t = random( (10, 1, tb.state_size // 2) )
+    a = random( (10, 1, tb.state_size // 2) )
+    e = tb.compute_error( a, t )
+    print( f"{e=}" )
